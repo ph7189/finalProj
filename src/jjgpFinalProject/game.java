c3a992ab67c45b5073cfb4b8daf148ef76d87190
ph7189 <39099435+ph7189@users.noreply.github.com>
2018-05-18 11:12:51 -0700

    Update game.java

--- src/jjgpFinalProject/game.java
+++ src/jjgpFinalProject/game.java
@@ -1,11 +1,17 @@
 package jjgpFinalProject;
 
+import java.awt.Color;
+import java.awt.Graphics;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 
 public class game{
 	
+	board board= new board();
+	
+	public enum Class{Mage, Paladin};
 	JFrame frame;
 	JPanel panel;
 	
@@ -17,11 +23,25 @@ public class game{
 		
 		frame = new JFrame("Hearthstone");
 		panel = new JPanel();
+		panel = new JPanel() {
+			@Override
+			public void paintComponent(Graphics g) {
+				super.paintComponent(g);
+				drawGame(g);
+			}
+		};
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.add(panel);
 		frame.pack();
-		frame.setSize(600, 600);
+		frame.setSize(1950, 1160);
 		frame.setVisible(true);
+		panel.setBackground(Color.BLACK);
+		frame.repaint();
+	}
+	
+	private void drawGame(Graphics g) {
+		
+		board.draw(g);
 	}
 
 }
