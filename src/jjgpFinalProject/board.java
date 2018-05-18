d7cb26671ee9f7b2ec5b916627e2d90bf5003f31
ph7189 <39099435+ph7189@users.noreply.github.com>
2018-05-18 11:11:37 -0700

    Update board.java

--- src/jjgpFinalProject/board.java
+++ src/jjgpFinalProject/board.java
@@ -1,9 +1,25 @@
 package jjgpFinalProject;
 
-public class board{
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.ImageObserver;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+
+public class board{
+	private Image image;
 	public board() {
-		// TODO Auto-generated constructor stub
+		try {
+			image= ImageIO.read(new File("src/board.png"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
+	public void draw(Graphics g) {
+		g.drawImage(image,0,0,null);
+	}
 }
